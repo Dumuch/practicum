374916e2c762159c87d28bb0a10f3ecc9dcd6705
kizin_d <dumuch@yandex.ru>
2023-10-23 09:02:57 +0800

    start

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
+import {resolve} from 'path'
 
 export default defineConfig({
-    plugins: [handlebars()],
+    server: {
+        port: 3000
+    },
+    root: resolve(__dirname, 'src'),
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'src/index.html'),
+                profile: resolve(__dirname, 'src/profile.html'),
+                signIn: resolve(__dirname, 'src/signIn.html'),
+                signUp: resolve(__dirname, 'src/signUp.html'),
+                norFound: resolve(__dirname, 'src/404.html'),
+                error: resolve(__dirname, 'src/5**.html')
+            }
+        },
+        outDir: resolve(__dirname, 'dist')
+    },
+    plugins: [handlebars({
+        partialDirectory: resolve(__dirname, 'src/partials')
+    })],
 })
