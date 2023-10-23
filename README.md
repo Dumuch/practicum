86f0e20fc6be7d93d911ffcfb01b36c8bb580886
kizin_d <dumuch@yandex.ru>
2023-10-23 14:42:18 +0800

    Adding form styles

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sprint_1
+
+
+- Ссылка на макет https://www.figma.com/file/jF5fFFzgGOxQeB4CmKWTiE/Chat_external_link?type=design&node-id=0-1&mode=design
  
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
-import {resolve} from 'path'
+import { resolve } from 'path'
 
 export default defineConfig({
     server: {
-        port: 3000
+        port: 3000,
+        proxy: {
+            '/index.html': '/pages/index/index.html'
+        }
     },
     root: resolve(__dirname, 'src'),
     build: {
@@ -14,13 +17,19 @@ export default defineConfig({
                 profile: resolve(__dirname, 'src/profile.html'),
                 signIn: resolve(__dirname, 'src/signIn.html'),
                 signUp: resolve(__dirname, 'src/signUp.html'),
-                norFound: resolve(__dirname, 'src/404.html'),
-                error: resolve(__dirname, 'src/5**.html')
+                notFound: resolve(__dirname, 'src/404.html'),
+                error: resolve(__dirname, 'src/500.html')
             }
-        },
+},
         outDir: resolve(__dirname, 'dist')
     },
     plugins: [handlebars({
-        partialDirectory: resolve(__dirname, 'src/partials')
+        partialDirectory: [
+            resolve(__dirname, 'src/partials/button'),
+            resolve(__dirname, 'src/partials/link'),
+            resolve(__dirname, 'src/partials/input'),
+            resolve(__dirname, 'src/partials/form'),
+        ]
     })],
+
 })
