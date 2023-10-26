a133832bda8e950814bdc7a571f5d72103fdd45a
kizin_d <dumuch@yandex.ru>
2023-10-26 11:49:57 +0800

    f

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
     server: {
-        port: 3000,
+        port: 4000,
         open: '/pages/signIn/signIn.html',
     },
     root: resolve(__dirname, 'src'),
