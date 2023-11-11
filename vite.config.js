374f22afd7059dd02418e4c29d68493ce23057c7
kizin_d <dumuch@yandex.ru>
2023-11-11 22:38:41 +0800

    Подклчючение eslint, tsConfig, prettier

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
-import { resolve } from 'path'
+import { resolve } from 'path';
 
 export default defineConfig({
-    server: {
-        port: 3000,
-        open: '/pages/signIn/signIn.html',
+  server: {
+    port: 3000,
+    open: '/pages/signIn/signIn.html',
+  },
+  root: resolve(__dirname, 'src'),
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/pages/index/index.html'),
+        chat: resolve(__dirname, 'src/pages/chat/chat.html'),
+        profile: resolve(__dirname, 'src/pages/profile/profile.html'),
+        signIn: resolve(__dirname, 'src/pages/signIn/signIn.html'),
+        signUp: resolve(__dirname, 'src/pages/signUp/signUp.html'),
+        notFound: resolve(__dirname, 'src/pages/404/404.html'),
+        error: resolve(__dirname, 'src/pages/500/500.html'),
+      },
     },
-    root: resolve(__dirname, 'src'),
-    build: {
-        rollupOptions: {
-            input: {
-                index: resolve(__dirname, 'src/pages/index/index.html'),
-                chat: resolve(__dirname, 'src/pages/chat/chat.html'),
-                profile: resolve(__dirname, 'src/pages/profile/profile.html'),
-                signIn: resolve(__dirname, 'src/pages/signIn/signIn.html'),
-                signUp: resolve(__dirname, 'src/pages/signUp/signUp.html'),
-                notFound: resolve(__dirname, 'src/pages/404/404.html'),
-                error: resolve(__dirname, 'src/pages/500/500.html')
-            }
-        },
-        outDir: resolve(__dirname, 'dist')
-    },
-    plugins: [handlebars({
-        partialDirectory: [
-            resolve(__dirname, 'src/layouts/precompiledLayout'),
-        ],
-        context: 'global'
-    })],
-
-})
+    outDir: resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: [resolve(__dirname, 'src/layouts/precompiledLayout')],
+      context: 'global',
+    }),
+  ],
+});
