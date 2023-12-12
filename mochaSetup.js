6f78a3f1b991142e5412069d16ee422cf0e4d546
kizin_d <dumuch@yandex.ru>
2023-12-12 16:22:20 +0800

    Написаны тесты для Router

---
+++ mochaSetup.js
@@ -0,0 +1,9 @@
+import { JSDOM} from "jsdom"
+
+const jsdom = new JSDOM('<body></body>', {
+    url: 'http://example.org'
+})
+
+global.window = jsdom.window
+global.document = jsdom.window.document
+global.FormData = jsdom.window.FormData
